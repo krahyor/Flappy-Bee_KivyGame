83963a4ef27dca152629269c34cb919bef3b7260
Nopphakao <6510110218@email.psu.ac.th>
2023-02-08 01:36:59 +0700

    🛠set cloud

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.uix.image import Image
-from kivy.core.window import Window
+from kivy.core.window import Window #import window
 
 class Background(Widget):
     cloud_texture = ObjectProperty(None)
@@ -10,9 +10,9 @@ class Background(Widget):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self.cloud_texture = Image(source="cloud.png").texture
-        self.cloud_texture.wrap = 'repeat'
-        self.cloud_texture.uvsize = (Window.width/self.cloud_texture.width,-1)
+        self.cloud_texture = Image(source="cloud.png").texture # เพิ่มรูป
+        self.cloud_texture.wrap = 'repeat' #เป็นการทำซ้ำ
+        self.cloud_texture.uvsize = (Window.width/self.cloud_texture.width,-1) # บอกขนาด
 
 
     pass

9023baeaf1bc2d91731808d8210b1a5aaf522bb6
Nopphakao <6510110218@email.psu.ac.th>
2023-02-08 01:32:53 +0700

    🔁change cloud

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.uix.image import Image
+from kivy.core.window import Window
 
 class Background(Widget):
     cloud_texture = ObjectProperty(None)
@@ -10,6 +11,9 @@ class Background(Widget):
         super().__init__(**kwargs)
 
         self.cloud_texture = Image(source="cloud.png").texture
+        self.cloud_texture.wrap = 'repeat'
+        self.cloud_texture.uvsize = (Window.width/self.cloud_texture.width,-1)
+
 
     pass
 

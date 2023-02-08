e0323bbf464f76388df55d50aa9293e577247533
psu6510110004 <dragonslayertitle@gmail.com>
2023-02-08 16:58:32 +0700

    สร้างตัวกำหนดความสูง ความกว้างของ Pipe

--- Program/pipe.py
+++ Program/pipe.py
@@ -1,7 +1,15 @@
 from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty
 
 class Pipe(Widget):
+    GAP_SIZE = NumericProperty(60)
+    CAP_SIZE = NumericProperty(0)          # ความสูงของหัวท่อ
+    bottom_body_position = NumericProperty(0)
+    bottom_cap_position = NumericProperty(0)
+    top_body_position = NumericProperty(0)
+    top_cap_position = NumericProperty(0)
     
+    #Texture
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
